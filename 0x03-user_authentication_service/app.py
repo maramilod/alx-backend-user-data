05c7c01124097c3313b0425a5f84257aa3ad42b6
maramilod <marammilod03@gmail.com>
2024-06-13 22:22:28 +0100

    add

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+simple flask app
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    ''' first route'''
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    ''' second route'''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    ''' third route'''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user = AUTH.valid_login(email, password)
+    if not user:
+        abort(401)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", AUTH.create_session(email))
+    return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    ''' fourth route '''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route("/profile")
+def profile():
+    ''' fifth route '''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    ''' sixth route '''
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    ''' sixth route '''
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+auth module
+"""
+from typing import Union
+import uuid
+import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialize a new Auth instance
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            pass
+
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+        return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        user.session_id = _generate_uuid()
+        self._db._session.commit()
+        return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Get a user from a session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ sets the users session id to None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ get a reaset password token using email  """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ resets passowrd using the reset token if it exists
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a new UUID
+    """
+    return str(uuid.uuid4())
