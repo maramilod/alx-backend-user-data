9a8b11a761c483b5b31548517593aa87b818fe9b
maramilod <marammilod03@gmail.com>
2024-07-12 11:13:22 +0100

    add

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-h
-e
-y
+Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
@@ -23,6 +21,15 @@ if AUTH_TYPE == "auth":
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -53,14 +60,18 @@ def before_request() -> str:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if not auth or not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    cokkie = auth.session_cookie(request)
+    auth_header = auth.authorization_header(request)
+    if auth_header is None and cokkie is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
